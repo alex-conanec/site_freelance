9ad83823fc7ece4676a84ebbd207bfbcad091f40
alex-conanec <alexandreconanec@gmail.com>
2022-04-19 10:47:53 +0200

    modify the speed of the carousel to 10sec/slide

--- src/components/Home.js
+++ src/components/Home.js
@@ -69,7 +69,7 @@ function Main() {
             <section id="trust2">
                 <h2>{content[lang].trust.title}</h2>
                 
-                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-interval="10000">
+                <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="10000">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
